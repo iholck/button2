b7a6ee8f7aa7a1c97f9099cf276cfe870b14c665
Ivar Holck <ivar.holck@gmail.com>
2019-11-07 13:07:19 +0100

    catch database errors

--- importer/server.js
+++ importer/server.js
@@ -12,10 +12,13 @@ const accessKey = process.env.TTNACCESSKEY
 // discover handler and open mqtt connection
 ttn.data(appID, accessKey)
   .then(function (client) {
-    client.on("uplink", function (devID, payload) {
+    return client.on("uplink", function (devID, payload) {
       console.log("Received uplink from ", devID)
       console.log(payload)
-      database.writeData(payload);
+      database.writeData(payload).catch(function (err) {
+        console.error(err)
+        process.exit(1)
+      });
     })
   })
   .catch(function (err) {
