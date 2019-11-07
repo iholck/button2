8c85fabb091165384e234ef3495ef1a2cbbd8c3c
Ivar Holck <ivar.holck@gmail.com>
2019-11-07 21:01:42 +0100

    .dockerignore file + date formatting in timeStamp

--- helpers/tools.js
+++ helpers/tools.js
@@ -1,7 +1,7 @@
 const Tools = {
     timeStamp: function(){
         const time = new Date(Date.now());
-        return `${time.getDate()}/${time.getMonth()}/${time.getFullYear()} ${time.getHours()}:${time.getMinutes()}.${time.getSeconds()}.${time.getMilliseconds()}`;
+        return `${time.getDate().toLocaleString(undefined, {minimumIntegerDigits: 2})}/${time.getMonth().toLocaleString(undefined, {minimumIntegerDigits: 2})}/${time.getFullYear()} ${time.getHours().toLocaleString(undefined, {minimumIntegerDigits: 2})}:${time.getMinutes().toLocaleString(undefined, {minimumIntegerDigits: 2})}.${time.getSeconds().toLocaleString(undefined, {minimumIntegerDigits: 2})}.${time.getMilliseconds().toLocaleString(undefined, {minimumIntegerDigits: 3})}`;
     },
     log: function(logInput){
         console.log(this.timeStamp()+' '+logInput);
