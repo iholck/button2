0b43ba2708f589503099ad4fe608078d7a282ca1
Ivar Holck <ivar.holck@gmail.com>
2019-12-02 15:38:39 +0100

    Backend to https

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 ﻿require('rootpath')();
 const express = require('express');
 const app = express();
+const https = require('https');
+const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
@@ -23,6 +25,9 @@ app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.BACKEND_PORT || 80) : 4000;
-const server = app.listen(port, function () {
+https.createServer({
+    key: fs.readFileSync('/run/secrets/server.key'),
+    cert: fs.readFileSync('/run/secrets/server.cert')
+  }, app) .listen(port, function () {
     console.log('Server listening on port ' + port);
 });
